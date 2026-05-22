140f83a3ee2a4fd3b28e552f05b395f2aa720af6
Nubia Tirabassi <nu.ragagnin91@gmail.com>
2026-05-22 16:41:44 -0300

    criando formularios e instanciando

---
+++ br.com.projeto.dao/FuncionarioDAO.cs
@@ -0,0 +1,298 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto_Controle_de_Vendas.br.com.projeto.conexaoDB;
+using Projeto_Controle_de_Vendas.br.com.projeto.model;
+using Projeto_Controle_de_Vendas.br.com.projeto.view;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Controle_de_Vendas.br.com.projeto.dao
+{
+    public class FuncionarioDAO
+    {
+
+        private MySqlConnection conexaoDataBase;
+
+        public FuncionarioDAO()
+        {
+            this.conexaoDataBase = new ConnectionFactory().getConnection();
+        }
+
+
+        #region Método Cadastrar Funcionario
+        public void cadastrarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                string sql = "insert into tb_funcionarios (nome,rg,cpf,email,senha,cargo,nivel_acesso,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)" +
+                             " values (@nome,@rg,@cpf,@email,@senha,@cargo,@nivel,@telefone,@celular,@cep,@endereco,@numero,@complemento,@bairro,@cidade,@estado)";
+            
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+
+                executacmd.Parameters.AddWithValue("@senha", obj.senha);
+                executacmd.Parameters.AddWithValue("@cargo", obj.cargo);
+                executacmd.Parameters.AddWithValue("@nivel", obj.nivel_acesso);
+
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                conexaoDataBase.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionário cadastrado com sucesso!");
+
+                conexaoDataBase.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+
+        }
+
+
+        #endregion
+
+        #region Método Alterar Funcionario
+        public void alterarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                string sql = "update tb_funcionarios set nome=@nome,rg=@rg,cpf=@cpf,email=@email,senha=@senha,cargo=@cargo,nivel_acesso=@nivel," +
+                                "telefone=@telefone,celular=@celular,cep=@cep,endereco=@endereco,numero=@numero," +
+                                "complemento=@complemento,bairro=@bairro,cidade=@cidade,estado=@estado where id = @codigo";
+           
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+
+                executacmd.Parameters.AddWithValue("@senha", obj.senha);
+                executacmd.Parameters.AddWithValue("@cargo", obj.cargo);
+                executacmd.Parameters.AddWithValue("@nivel", obj.nivel_acesso);
+
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                executacmd.Parameters.AddWithValue("@codigo", obj.codigo);
+
+                conexaoDataBase.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionário alterado com sucesso!");
+                conexaoDataBase.Close();
+
+            }
+            catch (Exception erro)
+            {
+
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+
+        }
+
+        #endregion
+
+        #region Método Excluir Funcionario
+        public void deletarFuncionario(Funcionario obj)
+        {
+            try
+            {
+                string sql = "delete from tb_funcionarios where id = @codigo";
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+
+                executacmd.Parameters.AddWithValue("@codigo", obj.codigo);
+
+                conexaoDataBase.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Funcionário excluido com sucesso!");
+                conexaoDataBase.Close();
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+            }
+
+        }
+
+        #endregion
+
+
+        #region Método ListarFuncionarios
+        public DataTable listarFuncionarios()
+        {
+            try
+            {
+                DataTable tabelafuncionario = new DataTable();
+                string sql = "select * from tb_funcionarios";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+                conexaoDataBase.Open();
+                executacmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(executacmd);
+                da.Fill(tabelafuncionario);
+
+                conexaoDataBase.Close();
+
+                return tabelafuncionario;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao executar o comando sql: " + erro);
+                return null;
+            }
+        }
+        #endregion
+
+        #region Método BuscaFuncionariosPorNome por Nome
+        public DataTable BuscaFuncionariosPorNome(string nome)
+        {
+            try
+            {
+                DataTable tabelafuncionario = new DataTable();
+                string sql = "select * from tb_funcionarios where nome = @nome";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+                executacmd.Parameters.AddWithValue("@nome", nome);
+                conexaoDataBase.Open();
+                executacmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(executacmd);
+                da.Fill(tabelafuncionario);
+
+                conexaoDataBase.Close();
+
+                return tabelafuncionario;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao executar o comando sql: " + erro);
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region Método que ListaFuncionariosPorNome
+        public DataTable listarFuncionariosPorNome(string nome)
+        {
+            try
+            {
+                DataTable tabelafuncionario = new DataTable();
+                string sql = "select * from tb_funcionarios where nome like @nome";
+
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+                executacmd.Parameters.AddWithValue("@nome", nome);
+                conexaoDataBase.Open();
+                executacmd.ExecuteNonQuery();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(executacmd);
+                da.Fill(tabelafuncionario);
+
+                conexaoDataBase.Close();
+
+                return tabelafuncionario;
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro ao executar o comando sql: " + erro);
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region Método que Efetua Login
+        public bool EfetuarLogin(string email, string senha)
+        {
+            try
+            {
+                string sql = @"select * from tb_funcionarios 
+                                     where email = @email and senha = @senha";
+                MySqlCommand executacmd = new MySqlCommand(sql, conexaoDataBase);
+                executacmd.Parameters.AddWithValue("@email", email);
+                executacmd.Parameters.AddWithValue("@senha", senha);
+
+                conexaoDataBase.Open();
+
+                MySqlDataReader reader = executacmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    string nivel = reader.GetString("nivel_acesso");
+                    string nome = reader.GetString("nome");
+
+                    MessageBox.Show("Seja bem vindo ao sistema, " + nome);
+
+                    FrmMenu telamenu = new FrmMenu();
+
+                    telamenu.txtusuario.Text = nome;
+
+                    if (nivel.Equals("Administrador"))
+                    {
+
+                        telamenu.Show();
+                    }
+
+                    else if (nivel.Equals("Vendedor"))
+                    {
+                        telamenu.menuProdutos.Visible = false;
+                        telamenu.menuHistorico.Enabled = false;
+                        telamenu.Show();
+                    }
+
+
+
+
+
+                    return true;
+                }
+
+                else
+                {
+                    MessageBox.Show("Email ou senha incorreto!");
+                    return false;
+                }
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Aconteceu o erro: " + erro);
+                return false;
+            }
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ br.com.projeto.view/FrmMenu.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Projeto_Controle_de_Vendas.br.com.projeto.view
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Frmfuncionarios tela = new Frmfuncionarios();
+            tela.ShowDialog();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            txtdata.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Programação dtentro do timer
+            //Pegando a hora
+            txthora.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void cadastroDeClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frmclientes tela = new Frmclientes();
+
+            tela.ShowDialog();
+        }
+
+        private void consultaDeClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frmclientes tela = new Frmclientes();
+            tela.tabClientes.SelectedTab = tela.tabPage2;
+            tela.ShowDialog();
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Frmfuncionarios tela = new Frmfuncionarios();
+            tela.tabFuncionario.SelectedTab = tela.tabPage2;
+            tela.ShowDialog();
+        }
+
+        private void cadastroDeFornecedoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frmfornecedores tela = new Frmfornecedores();
+            tela.ShowDialog();
+        }
+
+        private void consultaDeFornecedoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frmfornecedores tela = new Frmfornecedores();
+            tela.tabFornecedor.SelectedTab = tela.tabPage2;
+            tela.ShowDialog();
+        }
+
+        private void menuCadastroProdutos_Click(object sender, EventArgs e)
+        {
+            Frmprodutos tela = new Frmprodutos();
+            tela.ShowDialog();
+        }
+
+        private void consultaDeProdutosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frmprodutos tela = new Frmprodutos();
+            tela.tabProdutos.SelectedTab = tela.tabPage2;
+            tela.ShowDialog();
+        }
+
+        private void novaVendaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frmvendas tela = new Frmvendas();
+            tela.ShowDialog();
+        }
+
+        private void menuHistorico_Click(object sender, EventArgs e)
+        {
+            Frmhistorico tela = new Frmhistorico();
+            tela.ShowDialog();
+        }
+
+        private void sairDoSistemaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Você deseja Sair?", "ATENÇÂO!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void trocarDeUsuárioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Você deseja Trocar de usuário?", "ATENÇÂO!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                new Frmlogin().Show();
+                this.Dispose();
+            }
+        }
+    }
+}
